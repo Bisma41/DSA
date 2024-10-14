2ae1f485f3e5d4023b45b8815749eae40cb746b8
Bisma41 <152860495+Bisma41@users.noreply.github.com>
2024-10-14 21:44:39 +0500

    Update T1.cpp

--- Labs/lab 94/T1.cpp
+++ Labs/lab 94/T1.cpp
@@ -1,73 +1,42 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
 using namespace std;
 
-const int SIZE1 = 10;
-const int SIZE2 = 20;
-
-// Function to print an array
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
-        cout << setw(4) << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-// Function to initialize a sorted array
-void initializeSortedArray(int arr[], int size, int startValue) {
-    arr[0] = startValue;
-    for (int i = 1; i < size; i++) {
-        arr[i] = arr[i - 1] + rand() % 5;  // Next element is randomly 0-4 more than the previous element
-    }
-}
-
-// Function to merge two sorted arrays into a third sorted array
+        cout << arr[i] << " ";}
+    cout << endl;}
 void mergeArrays(int arr1[], int arr2[], int mergedArr[], int size1, int size2) {
     int i = 0, j = 0, k = 0;
-
-    // Merge the two arrays into one
-    while (i < size1 && j < size1) {
+    while (i < size1 && j < size2) {
         if (arr1[i] < arr2[j]) {
             mergedArr[k++] = arr1[i++];
         } else {
             mergedArr[k++] = arr2[j++];
         }
     }
-
-    // If there are remaining elements in arr1
     while (i < size1) {
         mergedArr[k++] = arr1[i++];
     }
-
-    // If there are remaining elements in arr2
-    while (j < size1) {
+    while (j < size2) {
         mergedArr[k++] = arr2[j++];
     }
 }
-
 int main() {
-    srand(time(0));  // Seed for random number generation
-
-    int arr1[SIZE1], arr2[SIZE1], mergedArr[SIZE2];
-
-    // Initialize two sorted arrays
-    initializeSortedArray(arr1, SIZE1, rand() % 10);  // Starting from a random number between 0-9
-    initializeSortedArray(arr2, SIZE1, rand() % 10 + 10);  // Starting from a random number between 10-19
-
-    // Merge the two arrays into the third array
-    mergeArrays(arr1, arr2, mergedArr, SIZE1, SIZE1);
-
-    // Print all three arrays
+    int S1 = 10;
+    int S2 = 10;
+    int MERGED_SIZE = S1 + S2;
+    int arr1[S1] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+    int arr2[S2] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int mergedArr[MERGED_SIZE];
     cout << "Array 1: ";
-    printArray(arr1, SIZE1);
-
+    printArray(arr1, S1);
     cout << "Array 2: ";
-    printArray(arr2, SIZE1);
-
+    printArray(arr2, S2);
+    mergeArrays(arr1, arr2, mergedArr, S1, S2);
     cout << "Merged Array: ";
-    printArray(mergedArr, SIZE2);
-
+    printArray(mergedArr, MERGED_SIZE);
     return 0;
 }
+
+
+

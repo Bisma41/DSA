25492d62010957f42604cb57d69ddd6f3390bb3f
BISMA SAJJAD <152860495+Bisma41@users.noreply.github.com>
2025-02-11 21:10:00 +0500

    Add files via upload

---
+++ code7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	srand(time(0));
+    int i, j, temp, x[100];
+	//Run this code multiple times and check the output will remain the same
+    for (i = 0 ; i < 100 ; i++){
+    	x[i] = rand() * rand(); 
+    	cout << x[i] << ' ';
+	}
+	cout << "\n\n";
+	for (i = 0 ; i < 100 ; i++)
+		for (j = 0 ; j < 99 ; j++)
+			if (x[j] < x[j+1]){
+				temp = x[j];
+				x[j] = x[j+1];
+				x[j+1] = temp;
+			} 
+    for (i = 0 ; i < 100 ; i++)
+    	cout << x[i] << ' ';
+	cout << '\n';
+    return 0;
+}

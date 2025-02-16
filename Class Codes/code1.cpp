3d331c0d7bba636f44c6b543079b189ae968543e
BISMA SAJJAD <152860495+Bisma41@users.noreply.github.com>
2025-02-16 23:54:18 +0500

    Update code1.cpp

--- Class Codes/code1.cpp
+++ Class Codes/code1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     char c = 126;
-    //we are casting char type to print integer value instead of character
+    //we aree casting char type to print integer value instead of character
     cout << (int)c << '\n';
 	c = c + 5;//value will exceed the range that is +127
     cout << (int)c << '\n';
